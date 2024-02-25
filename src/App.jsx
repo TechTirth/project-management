baf561b814efe8af5f6b35647a40f2debeb171d6
Tirth Vanparia <144980949+TechTirth@users.noreply.github.com>
2024-02-25 11:57:47 +0530

    making the default noProjects selected  section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProjects from "./components/NewProjects";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebars from "./components/ProjectsSidebars";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebars />
-      <NewProjects />
+      <NoProjectSelected />
     </main>
   );
 }
